7112d0afe37fbc6243559c6b39dfc469a9b40ee1
Quirijn Slings <quirijn.slings@trivident.com>
2015-04-09 18:01:12 +0200

    Created legacy build config, mock ECL lib, and the multimedia size is set with reflection

---
+++ source/Tridion.ExternalContentLibrary.V2.Mock/SessionFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tridion.ExternalContentLibrary.V2
+{
+    public class SessionFactory
+    {
+        public static IEclSession CreateEclSession(object templatingSession)
+        {
+            return null;
+        }
+    }
+}
