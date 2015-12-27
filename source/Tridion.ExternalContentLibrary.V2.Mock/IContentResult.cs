c7a6340a524db781bffa65fa15f0dddf695fb421
Quirijn Slings <quirijn.slings@trivident.com>
2015-12-27 13:38:40 +0100

    Added IContentResult

---
+++ source/Tridion.ExternalContentLibrary.V2.Mock/IContentResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tridion.ExternalContentLibrary.V2
+{
+    public interface IContentResult
+    {
+        string ContentType { get; set; }
+        System.IO.Stream Stream { get; set; }
+    }
+}
