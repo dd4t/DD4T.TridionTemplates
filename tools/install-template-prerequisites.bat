45b1cecfa6f9214dbc6f81c1c84752fd6afa36d4
Quirijn Slings <quirijn.slings@trivident.com>
2018-02-03 16:15:01 +0100

    Renamed private Tridion Nuget packages Merge only includes DD4T and Newtonsoft Changed private Nuget server to Trivident Downgraded from .NET 4.6.2 back to 4.5.2 to make it work on 2013

--- tools/install-template-prerequisites.bat
+++ tools/install-template-prerequisites.bat
@@ -1,4 +1,3 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-choco install visualstudio2015community -y
 choco install ilmerge -y
 choco install msbuild.communitytasks -y
